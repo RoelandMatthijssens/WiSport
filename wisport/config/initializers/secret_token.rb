e94a87617260a4e1e180db2a54ab13b23fe2d165
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2012-10-19 15:58:38 +0200

    new rails project

---
+++ wisport/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wisport::Application.config.secret_token = '488876dc96177ca15dd9c6829ab66964bec74d83ffc216184b8e3baa5102b16fb44c78239dd11ec5c162e8b6c030d01085e08ebc842d2f21eedafdce5dbb2016'
