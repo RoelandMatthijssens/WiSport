6c1615b459d7ac1629fe5acb39ec1c81e3b920d1
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2012-10-31 14:28:21 +0100

    changes to deploy file

--- wisport/config/deploy.rb
+++ wisport/config/deploy.rb
@@ -46,19 +46,8 @@ namespace :deploy do
     run "touch #{deploy_to}/current/tmp/restart.txt"
   end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/apache.conf /etc/apache2/sites-available/#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
-  end
-  after "deploy:setup", "deploy:setup_config"
-  
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
-  
+  after "deploy:setup"
+    
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
