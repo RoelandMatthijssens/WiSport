2e0edaaa9947f160be02d12ec8c0fe816f632f5e
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2012-10-31 14:29:38 +0100

    changes to deploy file

--- wisport/config/deploy.rb
+++ wisport/config/deploy.rb
@@ -46,8 +46,6 @@ namespace :deploy do
     run "touch #{deploy_to}/current/tmp/restart.txt"
   end
 
-  after "deploy:setup"
-    
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
