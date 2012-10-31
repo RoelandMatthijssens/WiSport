62ab500248e07a0343b8cf17d89a9f62e9798cd8
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2012-10-31 14:09:13 +0100

    deploy setup

---
+++ wisport/config/deploy.rb
@@ -0,0 +1,71 @@
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+server "134.184.49.8", :web, :app, :db, primary: true
+
+set :application, "wisport"
+set :user, "webandinfosys_deploy"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository,  "git@github.com:rikvanmechelen/web_info_sys_sport.git"
+set :branch, "master"
+
+# https://rvm.io/integration/capistrano/
+set :rvm_ruby_string, "ruby-1.9.3-p286@#{application}"
+set :rvm_type, :system  # Copy the exact line. I really mean :system here
+require "rvm/capistrano"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+# if you want to clean up old releases on each deploy uncomment this:
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :restart, roles: :app, except: {no_release: true} do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/apache.conf /etc/apache2/sites-available/#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+  
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+  
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
