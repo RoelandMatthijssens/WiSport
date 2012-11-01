fda76b97ce40f84706701311e5709dc8a685b174
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2012-11-01 12:48:43 +0100

    for deploy bcrypt

--- wisport/config/deploy.rb
+++ wisport/config/deploy.rb
@@ -59,3 +59,13 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
+
+desc "tail production log files" 
+task :tail_logs, :roles => :app do
+  run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+    trap("INT") { puts 'Interupted'; exit 0; } 
+    puts  # for an extra line break before the host name
+    puts "#{channel[:host]}: #{data}" 
+    break if stream == :err
+  end
+end
