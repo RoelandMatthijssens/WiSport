09ac21a0738b1f5ad89eb6f7525ecc8c622457cd
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2012-12-19 16:41:23 +0100

    fixed graphs

--- wisport/app/models/do_exercise.rb
+++ wisport/app/models/do_exercise.rb
@@ -5,35 +5,47 @@ class DoExercise < ActiveRecord::Base
   attr_accessible :distance, :hours, :minutes, :reps, :seconds, :visibility, :do_session_id, :exercise_id, :user_id, :done_at
 
 	def value
+		hours ||= 0
+		minutes ||= 0
+		seconds ||= 0
 		if distance && distance != 0
 			return distance
 		else 
 			if reps && reps != 0
 				return reps
-			else if (minutes && hours && seconds)
-					return 3600*hours+60*minutes+seconds
-				else
-					return 0
-				end
+			else
+				return 3600*hours+60*minutes+seconds
 			end
 		end
 	end
 
 	def time
-		#if !hours || !minutes || !seconds
-			return 0
-		#else
-		#	return (3600*hours+60*minutes+seconds/goal())*100
-		#end
+		logger.debug("lala1 "+hours.inspect)
+		logger.debug("lala2 "+minutes.inspect)
+		logger.debug("lala3 "+seconds.inspect)
+		if !hours
+			mhours = 0
+		else
+			mhours = hours
+		end
+		if !minutes
+			mminutes = 0
+		else
+			mminutes = minutes
+		end
+		if !seconds
+			mseconds = 0
+		else
+			mseconds = seconds
+		end
+		logger.debug("lala1 "+mhours.inspect)
+		logger.debug("lala2 "+mminutes.inspect)
+		logger.debug("lala3 "+mseconds.inspect)
+		return (3600*mhours+60*mminutes+mseconds)/600
 	end
 
 	def time_goal
 		return 100
-	end
-
-	def goal
-		#
-		#return 3600*exercise.hours+60*exercise.minutes+exercise.seconds
 	end
 
 	validate :user, :exercise, presence: true
